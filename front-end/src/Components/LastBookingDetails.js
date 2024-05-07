f305456371e59de4719199ba1e54271c55b2f2ee
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-07 21:20:04 +0530

    updated

--- front-end/src/Components/LastBookingDetails.js
+++ front-end/src/Components/LastBookingDetails.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { seats } from '../data'
+import '../CSS/lastBookingDetails.css'
 
 const LastBookingDetails = () => {
-  return (
-    <div>LastBookingDetails</div>
+    return (
+        <div className='last-booking-details-main-container'>
+            <h2 className='last-booking-details-heading'>Last Booking</h2>
+
+            <div className='seats-container'>
+                <p className='seats-header'>Seats:</p>
+                <ul className='seats'>
+                    {seats.map((seats, index) => (
+                        <li className="seat-value" key={index}>
+                            (seat): 0
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            
+            <p className='slot' style={{textAlign:"left"}}>Slot: 00:00 AM<span></span></p>
+            <p className='movie'>Movie: <span>JOKER 2</span></p>
+        </div>
   )
 }
 
