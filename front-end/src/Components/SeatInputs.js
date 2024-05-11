cbeacacf8f09253bfa3167468102af8cae03ef90
Het Mamtora <104263376+HetMamtora@users.noreply.github.com>
2024-05-11 17:48:00 +0530

    updated

--- front-end/src/Components/SeatInputs.js
+++ front-end/src/Components/SeatInputs.js
@@ -20,14 +20,6 @@ const SeatInputs = ({key,text,noOfSeat,changeNoOfSeats,changeSeats,seat,index}) 
     }
 
     return (
-        /*
-        <div className='form-check-label'>
-            <span className='text'>{text}</span>
-            <input type='number' className='seats-input' placeholder='0' max='15' min='0'
-                   name={text} onChange={change_seats} value={noOfSeat[text]}
-            />
-
-        </div>*/
         <div name={text} 
              className={`form-check-label seats ${seat === text ? "active" : "inactive"}`}
              id={`${index}text`}
